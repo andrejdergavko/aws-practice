1d5549b0c29f3fd55244049b66043aa6e68825b1
andrejdergavko <andrejdergavko@gmail.com>
2026-07-02 15:56:24 +0300

    f

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <button className="bg-blue-500 text-white p-2 rounded-md">
-        Upload image
+        Upload image 111
       </button>
     </div>
   );
